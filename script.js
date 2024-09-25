4f27d9cb23cd4071d2773085f0e76c0ed8fb774d
Kaua Gomes Pereira <113049135+kauagg@users.noreply.github.com>
2024-09-25 09:26:50 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-const display = document.getElementById('clock')
+const display = document.getElementById('clock');
 const audio = new Audio('Alarm_Sound/biozard.mp3');
 audio.loop = true;
 let alarmTime = null;
 let alarmTimeout = null;
 
-
 function updateTime() {
     const date = new Date();
-
     const hour = formatTime(date.getHours());
     const minutes = formatTime(date.getMinutes());
     const seconds = formatTime(date.getSeconds());
-
     display.innerText = `${hour} : ${minutes} : ${seconds}`;
 }
+
 function formatTime(time) {
-    if ( time < 10 ){
-        return '0' + time;
-    }
-    return time;
+    return time < 10 ? '0' + time : time;
 }
 
 function setAlarmTime(value) {
-    alarmTime = value;
-    console.log('AlarmTime:', alarmTime);
+    const parsedTime = new Date(value);
+    if (!isNaN(parsedTime.getTime())) {
+        alarmTime = parsedTime;
+        console.log('AlarmTime:', alarmTime);
+    } else {
+        alert('Invalid alarm time');
+    }
 }
-
 
 function setAlarm() {
     if (alarmTime) {
         const current = new Date();
-        const timeToAlarm = new Date(alarmTime);
-        
-        if (timeToAlarm > current) {
+        if (alarmTime > current) {
             if (alarmTimeout) {
                 clearTimeout(alarmTimeout);
             }
-            const timeout = timeToAlarm.getTime() - current.getTime();
-            alarmTimeout = setTimeout(() => audio.play(), timeout);
+            const timeout = alarmTime.getTime() - current.getTime();
+            alarmTimeout = setTimeout(() => {
+                audio.play();
+                alert('Alarm ringing!');
+            }, timeout);
             alert('Alarm set');
         } else {
             alert('The alarm time is in the past.');
@@ -59,5 +59,5 @@ function clearAlarm() {
         alert('No alarm to clear');
     }
 }
-setInterval(updateTime, 1000)
 
+setInterval(updateTime, 1000);
